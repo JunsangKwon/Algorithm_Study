5169b88b36fe65f1ba08fd169e9d4a36d18ce7e4
권준상 <wnstkdsun97@naver.com>
2021-08-25 00:09:30 +0900

    BOJ_10815 숫자 카드

---
+++ BOJ/이분탐색/숫자 카드.py
@@ -0,0 +1,31 @@
+
+from sys import stdin, stdout
+
+input = stdin.readline
+
+
+def binary_search(array, target, start, end):
+    while(start <= end):
+        mid = (start + end) // 2
+        if array[mid] == target:
+            return 1
+        elif array[mid] > target:
+            end = mid-1
+        else:
+            start = mid + 1
+    return 0
+
+
+answer = []
+n = int(input())
+a_list = list(map(int, input().split()))
+a_list.sort()
+m = int(input())
+b_list = list(map(int, input().split()))
+
+for i in range(m):
+    target = b_list[i]
+    answer.append(binary_search(a_list, target, 0, n-1))
+
+for a in answer:
+    print(a, end=' ')

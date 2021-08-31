be5aeb7f2aa3875c90e6aeec87129431c54c7cbc
권준상 <wnstkdsun97@naver.com>
2021-08-31 21:22:03 +0900

    BOJ_2535 아시아 정보올림피아드

--- BOJ/정렬/아시아 정보올림피아드.py
+++ BOJ/정렬/아시아 정보올림피아드.py
@@ -4,9 +4,28 @@ input = stdin.readline
 
 records = []
 n = int(input())
+winners = []
 
 for i in range(n):
     record = list(map(int, input().split()))
     records.append(record)
 
-records.sort()
+records.sort(key=lambda x: (-x[2], x[0], x[1]))
+winners.append([records[0][0], records[0][1]])
+winners.append([records[1][0], records[1][1]])
+
+if(records[0][0] == records[1][0]):
+    country = records[0][0]
+    for i in range(2, len(records)):
+        if(records[i][0] == country):
+            continue
+        else:
+            winners.append([records[i][0], records[i][1]])
+            break
+else:
+    winners.append([records[2][0], records[2][1]])
+
+for i in range(len(winners)):
+    print("%d %d" % (winners[i][0], winners[i][1]))
+
+print(records)

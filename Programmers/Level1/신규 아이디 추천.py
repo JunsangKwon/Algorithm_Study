6633dfb99dc26e4d74510f9def119befe510efaa
권준상 <wnstkdsun97@naver.com>
2022-06-20 16:12:25 +0900

    PRO 신규 아이디 추천

--- Programmers/Level1/신규 아이디 추천.py
+++ Programmers/Level1/신규 아이디 추천.py
@@ -1,65 +1,52 @@
 def solution(new_id):
-    trigger = False
-    idlist = ['A']*len(new_id)
+    answer = ''
 
     # 1단계
     new_id = new_id.lower()
 
-    # 2단계 (문제 있음)
+    # 2단계
+    tmp_id = ''
     for i in range(len(new_id)):
-        idlist[i] = new_id[i]
+        if new_id[i].isalpha() or new_id[i] == '-' or new_id[i] == '_' or new_id[i] == '.' or new_id[i].isdigit():
+            tmp_id += new_id[i]
 
-    new_id = ''
-
-    for i in range(len(idlist)):
-        if(ord(idlist[i]) >= 97 and ord(idlist[i]) <= 122):
-            continue
-        if(ord(idlist[i]) >= 48 and ord(idlist[i]) <= 57):
-            continue
-        if(idlist[i] == '-' or idlist[i] == '_' or idlist[i] == '.'):
-            continue
-        idlist[i] = 'A'
-
-    for i in range(len(idlist)):
-        if(idlist[i] == 'A'):
-            idlist[i] = ""
+    new_id = tmp_id
 
     # 3단계
-    for s in idlist:
-        if(trigger):
-            if(s == '.'):
-                idlist.remove(s)
-            trigger = False
-        if(s == '.'):
-            trigger = True
+    stack = []
+    for i in range(len(new_id)):
+        stack.append(new_id[i])
+        if i == len(new_id) - 1:
+            break
+        elif stack[-1] == new_id[i+1] == '.':
+            stack.pop()
 
-    for i in range(len(idlist)):
-        new_id += idlist[i]
-
-    print(new_id)
+    new_id = ''.join(stack)
 
     # 4단계
-    if(new_id != ""):
-        if(new_id[0] == '.'):
-            new_id = new_id.replace(new_id[0], "")
-        elif(new_id[-1] == '.'):
-            new_id = new_id.replace(new_id[-1], "")
+    if len(new_id) >= 2:
+        if new_id[0] == '.':
+            new_id = new_id[1:]
+
+        if new_id[-1] == '.':
+            new_id = new_id[:-1]
+    elif len(new_id) == 1:
+        if new_id[0] == '.':
+            new_id = ''
 
     # 5단계
-    if(new_id == ""):
-        new_id = "a"
+    if len(new_id) == 0:
+        new_id += 'a'
 
     # 6단계
-    if(len(new_id) >= 16):
-        new_id = new_id[0:15]
-        if(new_id[-1] == '.'):
-            new_id = new_id.replace(new_id[-1], "")
+    if len(new_id) >= 16:
+        new_id = new_id[:15]
+        if new_id[-1] == '.':
+            new_id = new_id[:-1]
 
     # 7단계
-    if(len(new_id) <= 2):
-        if(len(new_id) == 1):
-            new_id = new_id * 3
-        else:
+    if len(new_id) <= 2:
+        while len(new_id) < 3:
             new_id += new_id[-1]
 
     return new_id
